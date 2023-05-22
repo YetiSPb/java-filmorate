0a2d3500934236c27bdda21aa881ee5def280aec
Fedotov <fedotov-evgeny@mail.ru>
2023-05-22 20:58:09 +0300

    Работа с пользователями и фильмами
    Созданы объекты
    - Film
    - User
    
    Созданы контроллеры для работы с ними
    - FilmController
    - UserController
    
    Созданы тесты для контроллеров
    - FilmControllerTest
    - UserControllerTest
    
    Took 12 minutes

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,85 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.controller.exception.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+    private static final LocalDate MIN_DATE_RELEASE = LocalDate.of(1895, 12, 28);
+
+    private final Map<Integer, Film> films = new LinkedHashMap<>();
+    private int countId = 0;
+
+    @PostMapping()
+    public ResponseEntity<Film> addFilm(@RequestBody @NotNull Film film) {
+
+        try {
+            generalValidateFilm(film);
+            film.setId(genNewId());
+            films.put(film.getId(), film);
+            log.info("Добавлен фильм: " + film);
+
+        } catch (ValidationException e) {
+            return ResponseEntity.badRequest().body(film);
+        }
+        return ResponseEntity.ok(film);
+    }
+
+    @PutMapping()
+    public ResponseEntity<Film> updateFilm(@RequestBody @NotNull Film film) throws ValidationException {
+
+        if (films.get(film.getId()) == null) {
+            throw new ValidationException("Фильм не найден!");
+        }
+
+        try {
+            generalValidateFilm(film);
+            films.remove(film.getId());
+            films.put(film.getId(), film);
+            log.info("Обновлен фильм: " + film);
+
+        } catch (ValidationException e) {
+            return ResponseEntity.badRequest().body(film);
+        }
+        return ResponseEntity.ok(film);
+    }
+
+    @GetMapping()
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    private int genNewId() {
+        return ++countId;
+    }
+
+    private void generalValidateFilm(@NotNull Film film) throws ValidationException {
+
+        if (film.getName() == null || film.getName().equals(""))
+            throw new ValidationException("Название не может быть пустым!");
+
+        if (film.getDescription().length() > 200)
+            throw new ValidationException("Максимальная длина описания — 200 символов!");
+
+        if (film.getReleaseDate().isBefore(FilmController.MIN_DATE_RELEASE)) {
+            throw new ValidationException("Дата релиза — не раньше 28.12.1985 !");
+        }
+        if (film.getDuration() < 1) {
+            throw new ValidationException("Продолжительность фильма должна быть положительной!");
+        }
+
+    }
+
+}
